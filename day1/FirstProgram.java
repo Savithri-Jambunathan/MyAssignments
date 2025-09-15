f2b10c40a6a9282a950a2233ac9183674d4ef3a9
Savithri-Jambunathan <savithri.jambunathan@gmail.com>
2025-09-15 16:28:44 -0400

    Add files via upload

---
+++ day1/FirstProgram.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package week1.day1;
+
+/**
+ * @author ramsu
+ *
+ */
+//public means access modifier. Class is the keyword. FirstProgram is the ClassName
+
+public                          class                  FirstProgram {
+	//create a main method
+	public static void main(String args[]) {
+	
+System.out.println("Welcome to TestLeaf. This is my first class in TestLeaf.");
+}}
+//what is a class? It is a Blue print to write the program.
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class LearnDataTypes
+{
+public static void main(String[] args)
+{
+	int number=9;
+	int amount=10000;
+	int depositAmount=50000;
+    long mobileNumber=6560595000L;
+    float rateOfInterest=7.25F;
+    System.out.println(rateOfInterest);
+    
+    float rupee=1.1234786f;
+    System.out.println(rupee);
+    
+    double dollar=1.1234786;
+    System.out.println(dollar);
+	
+    char logoOfTestLeaf='T';
+    System.out.println(logoOfTestLeaf);
+    
+    boolean hadBreakFast=false;
+
+    String name="Savithri";
+    System.out.println(name);
+    
+    int ageOfSavithri=35;
+    System.out.println("Age of Savithri is: " + ageOfSavithri);
+    
+}
+}     
